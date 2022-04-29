93d310218299d468ddb4ab1610e00d9ad9096421
Edward Kim <34976254+edkim41222@users.noreply.github.com>
2022-04-28 23:28:13 -0400

    asd

--- web/routes.js
+++ web/routes.js
@@ -7,11 +7,7 @@ const getRandomInt = (max) => {
 }
 
 router.get('/', (req, res) => {
-    if (getRandomInt(10) < 2) {	
-		res.status(500).send('This Version is supposed to fail 20% of the time.');
-	} else {
-		res.send("<!DOCTYPE html><html><head><title>Version C</title></head><body><h1>Version C</h1><p>This is version C of our CIS 188 project.</p></body></html>");
-	}
+	res.send("<!DOCTYPE html><html><head><title>Version B</title></head><body><h1>Version B</h1><p>This is version B of our CIS 188 project.</p></body></html>");
 });
 
 export default router;
